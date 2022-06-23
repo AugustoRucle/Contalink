c2af5e87604110005d0b77496582c63b18cdb5b0
Augusto Ruiz Clemente <augusto@realhost.com.mx>
2022-06-22 22:43:02 -0500

    Update color status

--- src/pages/ListInvoices/components/InvoiceStatusTag/InvoiceStatusTag.jsx
+++ src/pages/ListInvoices/components/InvoiceStatusTag/InvoiceStatusTag.jsx
@@ -8,8 +8,25 @@ function TagStatusOrder({
     status,
 }) {
 
+    /**
+     * Get color status
+     * 
+     * @return {string}
+     */
+    const getColor = () => {
+        switch (status) {
+            case "Vigente":
+                return "processing";
+            case "Cancelado":
+                return "error";
+
+            default:
+                "success";
+        }
+    }
+
     return <Tag
-        color={status == "Vigente" ? "processing" : "success"}
+        color={getColor()}
     >
         {status}
     </Tag>
